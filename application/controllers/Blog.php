eb94c252e6dbca1ce34a96f0dbfdcd843129b6dc
danangsatya <danang.satyan@gmail.com>
2022-10-23 13:36:49 +0700

    penambahan view control

---
+++ application/controllers/Blog.php
@@ -0,0 +1,13 @@
+<?php 
+class Blog extends CI_Controller 
+{ 
+  function __construct() 
+  { 
+      parent::__construct(); 
+  } 
+   
+  function index(){ 
+      $this->load->view('blog_view'); 
+  } 
+   
+} 
