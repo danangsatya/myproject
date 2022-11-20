7e4493a389be9cb28bfa561d25cac86197a5c9e0
danangsatya <danang.satyan@gmail.com>
2022-11-20 23:19:50 +0700

    dashboard

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -2,21 +2,63 @@
 <html lang="en">
 
 <head>
-	<?php $this->load->view('admin/_partials/head.php') ?>
+	<?php $this->load->view('admin/_partials/head.php') ?>   
 </head>
 
-<body>
-	<main class="main">
-		<?php $this->load->view('admin/_partials/side_nav.php') ?>
+<body class="fix-header fix-sidebar card-no-border">
 
-		<div class="content">
-			<h1>Overview</h1>
-			<p><b>13</b> <span class="text-gray">Post</span></p>
-			<p><b>11</b> <span class="text-gray">Feedback</span></p>	
-			
+    <!-- ============================================================== -->
+    <!-- Main wrapper - style you can find in pages.scss -->
+    <!-- ============================================================== -->
+	<div id="main-wrapper">
+		<main class="main">
+			<?php $this->load->view('admin/_partials/side_nav.php') ?>
+			<?php $this->load->view('admin/_partials/header.php') ?>
+			<div class="page-wrapper">
+			<div class="container-fluid">
+                <div class="row page-titles">
+                    <div class="col-md-5 col-8 align-self-center">
+                        <h3 class="text-themecolor m-b-0 m-t-0">Profile</h3>
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
+                            <li class="breadcrumb-item active">Profile</li>
+                        </ol>
+                	</div>
+				</div>
+				<div class="content">
+					<h1>Overview</h1>
+					<p><b>13</b> <span class="text-gray">Post</span></p>
+					<p><b>11</b> <span class="text-gray">Feedback</span></p>	
+				</div>
+			</div>
 			<?php $this->load->view('admin/_partials/footer.php') ?>
-		</div>
-	</main>
+		</main>
+	</div>
+	<script src="../assets/plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap tether Core JavaScript -->
+    <script src="../assets/plugins/bootstrap/js/tether.min.js"></script>
+    <script src="../assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+    <!-- slimscrollbar scrollbar JavaScript -->
+    <script src="js/jquery.slimscroll.js"></script>
+    <!--Wave Effects -->
+    <script src="js/waves.js"></script>
+    <!--Menu sidebar -->
+    <script src="js/sidebarmenu.js"></script>
+    <!--stickey kit -->
+    <script src="../assets/plugins/sticky-kit-master/dist/sticky-kit.min.js"></script>
+    <!--Custom JavaScript -->
+    <script src="js/custom.min.js"></script>
+    <!-- ============================================================== -->
+    <!-- This page plugins -->
+    <!-- ============================================================== -->
+    <!-- chartist chart -->
+    <script src="../assets/plugins/chartist-js/dist/chartist.min.js"></script>
+    <script src="../assets/plugins/chartist-plugin-tooltip-master/dist/chartist-plugin-tooltip.min.js"></script>
+    <!--c3 JavaScript -->
+    <script src="../assets/plugins/d3/d3.min.js"></script>
+    <script src="../assets/plugins/c3-master/c3.min.js"></script>
+    <!-- Chart JS -->
+    <script src="js/dashboard1.js"></script>
 </body>
 
 </html>
