06065dc50e23307bf70bd6b44ee2867d075550e4
danang satya <danang.satyan@gmail.com>
2024-01-27 15:09:41 +0700

    menampilkan user yang login

--- application/controllers/admin/Dashboard.php
+++ application/controllers/admin/Dashboard.php
@@ -2,6 +2,17 @@
 
 class Dashboard extends CI_Controller
 {
+	function __construct(){
+
+		parent::__construct();
+
+		if($this->session->userdata('status') != "login"){
+
+						redirect(base_url("login"));
+
+		}
+	}
+
 	public function index()
 	{
 		$this->load->view('admin/dashboard.php');
